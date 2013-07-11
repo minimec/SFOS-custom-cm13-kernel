27786414cd1c1057935346c637d0ba82f42fa12c
Benjamin Tissoires <benjamin.tissoires@redhat.com>
2013-07-11 15:41:30 +0200

    Bluetooth: hidp: remove wrong send_report at init
    The USB hid implementation does retrieve the reports during the start.
    However, this implementation does not call the HID command GET_REPORT
    (which would fetch the current status of each report), but use the
    DATA command, which is an Output Report (so transmitting data from the
    host to the device).
    The Wiimote controller is already guarded against this problem in the
    protocol, but it is not conformant to the specification to set all the
    reports to 0 on start.
    
    Signed-off-by: Benjamin Tissoires <benjamin.tissoires@redhat.com>
    Reviewed-by: David Herrmann <dh.herrmann@gmail.com>
    Acked-by: Jiri Kosina <jkosina@suse.cz>
    Signed-off-by: Gustavo Padovan <gustavo.padovan@collabora.co.uk>
    
    Conflicts:
    	net/bluetooth/hidp/core.c

--- net/bluetooth/hidp/core.c
+++ net/bluetooth/hidp/core.c
@@ -751,17 +751,6 @@ static int hidp_parse(struct hid_device *hid)
 
 static int hidp_start(struct hid_device *hid)
 {
-	struct hidp_session *session = hid->driver_data;
-	struct hid_report *report;
-
-	list_for_each_entry(report, &hid->report_enum[HID_INPUT_REPORT].
-			report_list, list)
-		hidp_send_report(session, report);
-
-	list_for_each_entry(report, &hid->report_enum[HID_FEATURE_REPORT].
-			report_list, list)
-		hidp_send_report(session, report);
-
 	return 0;
 }
 
